30f2d6331559356cebcc28145fb8ec42db0f86e6
Lee <leedunham@gmail.com>
2020-10-17 00:04:54 -0400

    Updated README, userSetup behaviour only occurs in presence of a mayaHacks module.

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # maya_hacks
-Modified maya scripts to fix or extend existing behaviour of some maya scripts.
+This is for educational/learning purposes only.
 
-../scripts directory contents is expected to be in a Maya scripts path.
-This could be via copy & paste or utilising Maya Modules etc.
+Maya example scripts to fix or extend existing behaviour in maya.
+
+
+> :warning: The userSetup.py modifies the MAYA_SCRIPT_PATH on 
+> startup to ensure .mel scripts override Maya's own internal 
+> scripts whilst preserving the user's script directories.
+>
+> This only happens if distributed via Maya modules and 
+> the module name is "mayaHacks" (see [Distribution](#distribution)).
+
+
+### Distribution
+It would be possible to distribute as a Maya module. 
+1. Create a text file (.mod) with
+ `+ mayaHacks 1.0 <PATH>/maya_hacks/maya_hacks` - replacing `<PATH>`.
+2. Save the .mod file to a MAYA_MODULE_PATH directory.
+ (this would typically be /My Documents/maya/modules).
+
+
+### Documentation
+* [Maya - Distributing Multi-File Modules](https://help.autodesk.com/view/MAYAUL/2019/ENU/?guid=__developer_Maya_SDK_MERGED_Distributing_Maya_Plug_ins_Distributing_Multi_File_Modules_html)
+* [Maya - Maya module paths, folders and versions](https://help.autodesk.com/view/MAYAUL/2019/ENU/?guid=__developer_Maya_SDK_MERGED_Distributing_Maya_Plug_ins_Maya_module_paths_folders_and_html)
--- maya_hacks/scripts/userSetup.py
+++ maya_hacks/scripts/userSetup.py
@@ -9,27 +9,29 @@ import inspect
 import maya.cmds
 
 
-# __file__ isnt available here.
-current_directory = os.path.normpath(os.path.dirname(inspect.getfile(inspect.currentframe())))
+# Only modify the MAYA_SCRIPT_PATH if we're a registered Maya module.
+# Otherwise assume the user will manually manage the path themselves.
+if 'mayaHacks' in maya.cmds.moduleInfo(listModules=True):
 
-# Make a reasonable assumption on the maya/scripts directory we want to insert our path after.
-expected_leading_dir = os.path.normpath(
-    os.path.join(
-        maya.cmds.internalVar(userAppDir=True),
-        'scripts',
-    ),
-).lower()
+    # __file__ isnt available here.
+    current_directory = os.path.normpath(os.path.dirname(inspect.getfile(inspect.currentframe())))
 
+    # Make a reasonable assumption on the maya/scripts directory we want to insert our path after.
+    expected_leading_dir = os.path.normpath(
+        os.path.join(
+            maya.cmds.internalVar(userAppDir=True),
+            'scripts',
+        ),
+    ).lower()
 
-# Insert our current directory after the assumed maya/scripts directory
-maya_script_paths = os.environ['MAYA_SCRIPT_PATH'].split(os.pathsep)
-for i, path in enumerate(maya_script_paths):
-    if os.path.normpath(path).lower() == expected_leading_dir:
-        maya_script_paths.insert(i + 1, current_directory)
-        break
+    # Insert our current directory after the assumed maya/scripts directory
+    maya_script_paths = os.environ['MAYA_SCRIPT_PATH'].split(os.pathsep)
+    for i, path in enumerate(maya_script_paths):
+        if os.path.normpath(path).lower() == expected_leading_dir:
+            maya_script_paths.insert(i + 1, current_directory)
+            break
 
+    os.environ['MAYA_SCRIPT_PATH'] = os.pathsep.join(maya_script_paths)
 
-os.environ['MAYA_SCRIPT_PATH'] = os.pathsep.join(maya_script_paths)
-
-# Cleanup
-del current_directory, expected_leading_dir, maya_script_paths, path, i
+    # Cleanup
+    del current_directory, expected_leading_dir, maya_script_paths, path, i
